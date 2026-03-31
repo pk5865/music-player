743f850bbad521744fee9d68b0c5b063326749c6
Pon Kishore <kponkishore@gmail.com>
2026-03-31 16:34:17 +0530

    fixed songs api route

--- musicbackend/songs/urls.py
+++ musicbackend/songs/urls.py
@@ -1,3 +1,13 @@
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from .views import SongViewSet
+
+# router = DefaultRouter()
+# router.register(r'songs', SongViewSet)
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import SongViewSet
